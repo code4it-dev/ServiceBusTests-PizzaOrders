3de307615bfece0a3799c4fe4e71bf8cf42778f8
Davide Bellone <davide.bellone>
2021-05-20 11:16:50 +0200

    Add project files.

---
+++ PizzaOrderSender/Controllers/PizzaOrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaOrderSender.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaOrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(IEnumerable<PizzaOrder> order)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ PizzaOrderSender/PizzaOrder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PizzaOrderSender
+{
+    public class PizzaOrder
+    {
+        public string Name { get; set; }
+        public ushort Quantity { get; set; }
+    }
+}
